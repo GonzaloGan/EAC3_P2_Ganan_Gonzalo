432c36ed147dc10ac30d724ab14211070208f06c
Gonzalo Ganan Gonzalez <se4cganan@gmail.com>
2019-11-19 22:45:56 +0100

    Done

--- app/src/main/java/cat/ioc/eac3_p2_ganan_gonzalo/MapsActivity.java
+++ app/src/main/java/cat/ioc/eac3_p2_ganan_gonzalo/MapsActivity.java
@@ -1,17 +1,32 @@
 package cat.ioc.eac3_p2_ganan_gonzalo;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Objects;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+
+    private static  final LatLng BCN = new LatLng(41.3863862, 2.1954018);
 
     private GoogleMap mMap;
 
@@ -22,9 +37,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        Objects.requireNonNull(mapFragment).getMapAsync(this);
+        Button button = findViewById(R.id.actual_location_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickLocation();
+            }
+        });
     }
-
 
     /**
      * Manipulates the map once available.
@@ -38,10 +59,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+
+        float zoom = 13;
+        mMap.addMarker(new MarkerOptions().position(BCN).title("Work"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BCN, zoom));
+        enableMyLocation();
+
+    }
+
+    private void onClickLocation() {
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(BCN)
+                .title("Actual Location")
+                .snippet("The actual location is: "+ BCN.latitude + " " + BCN.longitude));
+        marker.showInfoWindow();
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length > 0
+                    && grantResults[0]
+                    == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation();
+            }
+        }
     }
 }
